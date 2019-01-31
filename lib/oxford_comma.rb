0716bea2c1d3a8cebcb27f944dd16633e1c35c49
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-01-31 10:04:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-  
+  if array.size == 1
+    string = array.join
+  elsif array.size == 2
+    string = array.join(" and ")
+  elsif array.size >= 3
+      if array.index
+    string = array.join
+  end
+  return string
 end
+
