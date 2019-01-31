6edf38716cbf6fa58f28b3baeeaa7c0a6e21165f
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-01-31 10:30:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2
     string = array.join(" and ")
   elsif array.size >= 3
-    array.last = "and #{array.last}" 
+    array[-1] = "and #{array[-1]}" 
     string = array.join(", ")
   else
     string = ""
