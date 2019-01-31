5d6030867cc5a5596ef5403264f8d560238692a8
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-01-31 10:26:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,15 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2
     string = array.join(" and ")
   elsif array.size >= 3
-    string = ""
-    range = 0..(array.size - 1)
-    for index in range
-      if index < array.size - 1
-        string << "#{array[index]}, "
-      else
-        string << "and #{array.last}"
-      end
-    end
+    array[-1] = 
   else
     string = ""
   end
